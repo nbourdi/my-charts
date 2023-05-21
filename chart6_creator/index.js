d54df3f48a1610c9381e1c1f9430a604228c4619
ntua-el19020 <115226054+ntua-el19020@users.noreply.github.com>
2023-05-21 13:40:06 +0300

    chart6 micro

---
+++ chart6_creator/index.js
@@ -0,0 +1,60 @@
+const { Kafka } = require('kafkajs');
+
+const kafka = new Kafka({
+  clientId: 'chart_6_consumer',
+  brokers: ['kafka:9092'],
+});
+
+const consumer = kafka.consumer({ groupId: 'chart6cons' });
+const producer = kafka.producer();
+
+const fs = require("fs");
+
+async function run() {
+  await consumer.connect();
+  await consumer.subscribe({ topic: 'chart_6', fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      const key = message.key.toString();
+      const value = JSON.parse(message.value.toString());
+
+      const { spawn } = require('child_process');
+      const pythonScriptPath = 'consumer.py';
+      var pythonArgs = [value.title1, value.title2]
+      for (let i = 0; i < value.data.length; i++) {
+        pythonArgs.push(value.data[i].category);
+        pythonArgs.push(value.data[i].value);
+      }
+
+      const pythonProcess = spawn('python3', [pythonScriptPath, pythonArgs]);
+
+      pythonProcess.stdout.on('data', async (data) => {
+        console.log(`stdout: ${data}`);
+        const message = {
+          key: 'key',
+          value: JSON.stringify({
+            svg: data
+          })
+        };
+        await producer.connect();
+        await producer.send({
+          topic: 'chart_to_database',
+          messages: [ message ]
+        });
+      });
+
+      pythonProcess.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+      });
+
+      pythonProcess.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+      });
+    }
+  });
+}
+
+run().catch(console.error);
+
+module.exports = { consumer };
