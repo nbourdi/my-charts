4f540dd8b92fceb88712cccd3d0facf2fd9df7f6
ntua-el19065 <el19065@mail.ntua.gr>
2023-05-26 16:41:25 +0300

    FrontEnd get user charts implemented

--- microservice06-frontend/frontend/src/webpages/UserCharts.js
+++ microservice06-frontend/frontend/src/webpages/UserCharts.js
@@ -4,61 +4,62 @@ import axios from 'axios';
 import UserContext from '../UserContext';
 
 function UserCharts() {
-  const [results, setResults] = useState();
+
+  const [user_charts, setUserCharts] = useState([]);
 
   const { user } = useContext(UserContext);
 
-  // if (user == null) {
-  //   return (
-  //     <div className="layout">
-  //       <h1>Looks like you're not authorized to view this page. </h1><br></br>
-  //        Please Sign In to use our service...
-  //     </div>
-  //   )
-  // }
-  const data = [
-    { column1: 'Row 1', column2: 'Value 1', column3: 'Value 2', column4: 'Value 3' },
-    { column1: 'Row 2', column2: 'Value 4', column3: 'Value 5', column4: 'Value 6' },
-    { column1: 'Row 3', column2: 'Value 7', column3: 'Value 8', column4: 'Value 9' },
-    // Add more rows as needed
-  ];
   useEffect(() => {
 
-    const getResults = () => {
-      if (results) {
-        return;
-      }
+    const getUserCharts = () => {
+
       console.log("fetch");
-      fetch("http://localhost:3000/login/success", {
+      const url = `http://localhost:9107/chart_from_database/get_charts/${user.googleaccount.emails[0].value}`;
+      fetch(url, {
         method: "GET",
-        credentials: "include",
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
-          "Access-Control-Allow-Credentials": true,
         },
       })
         .then((response) => {
           return response.json();
         })
         .then((resObject) => {
-          setResults(resObject.results);
+          console.log(resObject)
+
+          const data = [];
+
+          for (let i = 0; i < resObject.length; i++) {
+            const obj = resObject[i];
+            const dataobj = {
+              column1: obj.type,
+              column2: obj.chart_title,
+              column3: obj.date.split('T')[0]
+            };
+
+            data.push(dataobj);
+
+          }
+          console.log(data);
+          setUserCharts(data);
         })
         .catch((err) => {
           console.log(err);
         });
     };
-    getResults();
-  }, [results]);
+    getUserCharts();
+  }, []);
 
   const handleRowClick = () => {
     window.location.href = "http://localhost:3030/user/charts"
   };
+
   return (
     <div className='layout'>
       {user ? (<div>
         <h1>Your Saved Charts</h1>
-        <ScrollableTable data={data} onRowClick={handleRowClick} />
+        <ScrollableTable data={user_charts} onRowClick={handleRowClick} />
       </div>) : (
 
         <div className="layout">
