cffe932cf3f95215f7db2d9fe5e96bbf843833a1
ntua-el19020 <115226054+ntua-el19020@users.noreply.github.com>
2023-05-26 15:04:58 +0300

    bar chart total connection frontend

--- microservice06-frontend/frontend/src/webpages/create/bar_chart.js
+++ microservice06-frontend/frontend/src/webpages/create/bar_chart.js
@@ -1,11 +1,14 @@
 import React, { useContext } from "react";
-import BarChartCSV from "./bar_chart.csv"
+import BarChartCSV from "./bar_chart.csv";
 import UserContext from "../../UserContext";
 
+let svg;
 const CreateBar = () => {
   // drag state
   const [dragActive, setDragActive] = React.useState(false);
   const [imageAppended, setImageAppended] = React.useState(false);
+  const [title, setTitle] = React.useState("");
+
   // ref
   const inputRef = React.useRef(null);
   const { user } = useContext(UserContext);
@@ -13,15 +16,15 @@ const CreateBar = () => {
   if (user == null) {
     return (
       <div className="layout">
-        <h1>Looks like you're not authorized to view this page. </h1><br></br>
-         Please Sign In to use our service...
+        <h1>Looks like you're not authorized to view this page. </h1>
+        <br></br>
+        Please Sign In to use our service...
       </div>
-    )
+    );
   }
 
-  
   // handle drag events
-  const handleDrag = function(e) {
+  const handleDrag = function (e) {
     e.preventDefault();
     e.stopPropagation();
     if (e.type === "dragenter" || e.type === "dragover") {
@@ -30,9 +33,9 @@ const CreateBar = () => {
       setDragActive(false);
     }
   };
-  
+
   // triggers when file is dropped
-  const handleDrop = function(e) {
+  const handleDrop = function (e) {
     e.preventDefault();
     e.stopPropagation();
     setDragActive(false);
@@ -40,48 +43,53 @@ const CreateBar = () => {
       handleFiles(e.dataTransfer.files);
     }
   };
-  
+
   // triggers when file is selected with click
-  const handleChange = function(e) {
+  const handleChange = function (e) {
     e.preventDefault();
     if (e.target.files && e.target.files[0]) {
       handleFiles(e.target.files);
     }
   };
-  
-  const handleFiles = async function(files) {
+
+  const handleFiles = async function (files) {
     const formData = new FormData();
     formData.append("my_csv", files[0]);
-  
+
     try {
-      const response = await fetch("http://localhost:9103/my_charts1/chart1_parser", {
-        method: "POST",
-        body: formData,
-      });
-  
+      const response = await fetch(
+        "http://localhost:9103/my_charts1/chart1_parser",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+      console.log(user.googleaccount);
       if (response.ok) {
         const jsonResponse = await response.json();
         const { status, message } = jsonResponse;
-  
+
         if (status === "success") {
           // File uploaded successfully
           console.log("File uploaded successfully");
-  
+
           // Extract SVG string from the message attribute
           const svgBuffer = JSON.parse(message).svg.data;
           const uint8Array = new Uint8Array(svgBuffer);
           const decoder = new TextDecoder("utf-8");
           const svgString = decoder.decode(uint8Array);
+          svg = svgString
           console.log(svgString);
           // Render the SVG as an image
           const image = new Image();
-          image.src = "data:image/svg+xml;charset=utf-8," + encodeURIComponent(svgString);
-  
+          image.src =
+            "data:image/svg+xml;charset=utf-8," +
+            encodeURIComponent(svgString);
+
           // Append the image to the DOM
           const container = document.getElementById("image-container");
           container.appendChild(image);
           setImageAppended(true);
-          
         } else {
           // Handle error response
           console.error("File upload failed:", message);
@@ -98,53 +106,125 @@ const CreateBar = () => {
       // Handle error scenario, e.g., show an error message
     }
   };
-  
-// triggers the input when the button is clicked
+
+  // triggers the input when the button is clicked
   const onButtonClick = () => {
     inputRef.current.click();
   };
-  
+
+  // handles title input change
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  // handles saving the chart
+  const handleSaveChart = async () => {
+    try {
+      //console.log("JOINED");
+      const data = {
+        title: title,
+        type: "Bar",
+        email:  user.googleaccount.emails[0].value,
+        svg: svg
+      };
+      const response = await fetch(
+        "http://localhost:9106/chart_to_database/add_chart",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+
+      if (response.ok) {
+        // Chart saved successfully
+        console.log("Chart saved successfully");
+      } else {
+        // Handle error response
+        console.error("Failed to save the chart");
+        // Handle error scenario, e.g., show an error message
+      }
+    } catch (error) {
+      // Handle network error
+      console.error("Network error:", error);
+      // Handle error scenario, e.g., show an error message
+    }
+  };
+
   return (
     <div className="layout">
-    
-    <div> <h2> Create your bar chart! </h2> </div>
-    <div>
-    <form id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
-      <input ref={inputRef} type="file" id="input-file-upload" multiple={true} onChange={handleChange} />
-      <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? "drag-active" : "" }>
+      <div>
+        <h2>Create your bar chart!</h2>
+      </div>
+      <div className="title-box">
+        <input
+          type="text"
+          className="title-input"
+          value={title}
+          onChange={handleTitleChange}
+          placeholder="Enter title"
+        />
+      </div>
+      <div>
+        <form
+          id="form-file-upload"
+          onDragEnter={handleDrag}
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <input
+            ref={inputRef}
+            type="file"
+            id="input-file-upload"
+            multiple={true}
+            onChange={handleChange}
+          />
+          <label
+            id="label-file-upload"
+            htmlFor="input-file-upload"
+            className={dragActive ? "drag-active" : ""}
+          >
+            <div>
+              <p>Drop your .csv file here or</p>
+              <button className="upload-button" onClick={onButtonClick}>
+                Browse file explorer
+              </button>
+            </div>
+          </label>
+          {dragActive && (
+            <div
+              id="drag-file-element"
+              onDragEnter={handleDrag}
+              onDragLeave={handleDrag}
+              onDragOver={handleDrag}
+              onDrop={handleDrop}
+            ></div>
+          )}
+        </form>
         <div>
-          <p>Drop your .csv file here or</p>
-          <button className="upload-button" onClick={onButtonClick}>Browse file explorer</button>
-        </div> 
-      </label>
-      { dragActive && <div id="drag-file-element" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></div> }
-    </form>
-    <div>
-    <h3>Model your CSV on this example! </h3>
-    <a
-        href={BarChartCSV}
-        download="Example-Bar-Chart.csv"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <button className="button">Download example</button>
-      </a>
+          <h3>Model your CSV on this example!</h3>
+          <a
+            href={BarChartCSV}
+            download="Example-Bar-Chart.csv"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="button">Download example</button>
+          </a>
+        </div>
       </div>
-    </div>
 
-    <div> 
-    
-
-      <div id="image-container" style={{ border: "2px dashed grey" }}>
+      <div>
+        <div id="image-container" style={{ border: "2px dashed grey" }}></div>
+        {imageAppended && (
+          <button className="button" onClick={handleSaveChart}>
+            Save Chart
+          </button>
+        )}
       </div>
-      {imageAppended && (
-                <button className="button">Save</button>
-              )}
-      </div>
-      
-      
     </div>
   );
 };
 
-export default CreateBar; 
+export default CreateBar;
